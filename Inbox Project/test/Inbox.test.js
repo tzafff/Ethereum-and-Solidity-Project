383504952ab0e64baea4daa2f59a0f9353117eb3
tzafff <christzaf99@gmail.com>
2023-07-23 12:30:53 +0300

    Add deployment test and more test

--- Inbox Project/test/Inbox.test.js
+++ Inbox Project/test/Inbox.test.js
@@ -3,17 +3,38 @@ const ganache = require('ganache');
 const { Web3 } = require('web3');
 const web3 = new Web3(ganache.provider());
 
+const { abi, evm } = require('../compile');
+
+let accounts;
+let inbox;
 
 beforeEach(async () => {
     //Get list of all accounts;
     accounts = await web3.eth.getAccounts();
-    console.log(accounts)
-    //Use one of those accounts to deploy
+    
+    // Use one of those accounts to deploy
     // the contract
+    inbox = await new web3.eth.Contract(abi)
+        .deploy({
+            data: evm.bytecode.object,
+            arguments: ['Hi there'],
+        })
+        .send({from: accounts[0], gas:'1000000'});
 });
 
 describe('Inbox', () =>{
     it('deploys a contract', () =>{
-
+        assert.ok(inbox.options.address);
     });
+
+    it('has a default message', async () => {
+        const message = await inbox.methods.message().call();
+        assert.equal(message, 'Hi there');
+      });
+
+      it('can change the message',async () =>{
+        await inbox.methods.setMessage('bye').send({from: accounts[0]});
+        const message = await inbox.methods.message().call();
+        assert.equal(message,'bye');
+      });
 });
